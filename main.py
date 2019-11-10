003d97530a8be859cd1e833042b8f669f9719feb
sst52 <saitejplacements@gmail.com>
2019-11-10 22:33:13 +0530

    Road Image Resizing

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ Created on Sun Nov 10 22:06:44 2019
 import cv2 as cv
 import numpy as np
 
+roadImage = cv.imread("roadImage.jpg")
+cv.resize(roadImage,(500,500))
+cv.imshow("Testing Image", roadImage)
+#cv.resizeWindow(winname="Testing Image", size=(500,500))
+cv.waitKey(1000000000)
+cv.destroyAllWindows()
+
