488207856bec2ea6f82ba7c473ddf146835caaac
sst52 <saitejplacements@gmail.com>
2019-11-12 22:05:47 +0530

    master

--- main.py
+++ main.py
@@ -14,27 +14,28 @@ Created on Sun Nov 10 22:06:44 2019
 import cv2 as cv
 import numpy as np
 
-roadImage = cv.imread("roadImage.jpg")
-#cv.resize(roadImage,(500,500))
-roadImageBackup = np.copy(roadImage)
-gray = cv.cvtColor(roadImageBackup,cv.COLOR_RGB2GRAY)
-cv.imshow("Testing Road Image - Press Any Key to Continue", roadImage)
-#cv.resizeWindow(winname="Testing Image", size=(500,500))
-cv.waitKey(0)
-cv.destroyAllWindows()
-cv.imshow("Gray Scale Image - Press Any Key to Continue", gray)
-cv.waitKey(0)
-cv.destroyAllWindows()
-gaussianBlur = cv.GaussianBlur(gray, (5,5), 0)
-gaussianBlurColoredImage = cv.GaussianBlur(roadImage, (5,5), 0)
-cv.imshow("Gaussian Blured Image - Press Any Key to Continue", gaussianBlur)
-cv.waitKey(0)
-cv.destroyAllWindows()
-cv.imshow("Gaussian Blured Color Image - Press Any Key to Continue", gaussianBlurColoredImage)
-cv.waitKey(0)
-cv.destroyAllWindows()
-edgeDetection = cv.Canny(gray,50,150)
-cv.imshow("Edge Detected Image - Press Any Key to Continue", edgeDetection)
-cv.waitKey(0)
-cv.destroyAllWindows()
-ok
+
+def readImage(imageName):
+    img = cv.imread(imageName)
+    return img
+
+def showImage(img, msg):
+    cv.imshow(msg, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def grayImage(img):
+    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    showImage(gray, "Gray Scaled Image - Press Any Key to Continue")
+    return gray
+
+def GaussianBlur(img):
+    gaussianBlur = cv.GaussianBlur(img, (5,5), 0)
+    showImage(gaussianBlur, "Gaussian Blured Image - Press Any Key to Continue")
+    return gaussianBlur
+
+def edgeDetection(img):
+    canny = cv.Canny(img, 50,150)
+    showImage(canny, "Edge Detection Image - Press Any Key to Continue")
+    return canny
+
